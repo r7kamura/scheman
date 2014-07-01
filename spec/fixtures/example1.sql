0ab76e09ce75bfeac0e60a527c416ae324b4129a
Ryo Nakamura <r7kamura@gmail.com>
2014-07-01 22:19:29 +0900

    Add some example SQL schemata for testing

---
+++ spec/fixtures/example1.sql
@@ -0,0 +1,23 @@
+create table person (
+  person_id INTEGER PRIMARY KEY,
+  name varchar(20),
+  age integer,
+  weight double(11,2),
+  iq tinyint default '0',
+  description text,
+  UNIQUE KEY UC_age_name (age)
+) ENGINE=MyISAM;
+
+create unique index u_name on person (name);
+
+create table employee (
+  position varchar(50),
+  employee_id integer,
+  job_title varchar(255),
+  CONSTRAINT FK5302D47D93FE702E FOREIGN KEY (employee_id) REFERENCES person (person_id),
+  PRIMARY KEY  (position, employee_id) USING BTREE
+) ENGINE=InnoDB;
+
+create table deleted (
+  id integer
+);
---
+++ spec/fixtures/example2.sql
@@ -0,0 +1,25 @@
+create table person (
+  person_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name varchar(20) not null,
+  age integer default '18',
+  weight double(11,2),
+  iq int default '0',
+  is_rock_star tinyint default '1',
+  physical_description text,
+  UNIQUE KEY UC_person_id (person_id),
+  UNIQUE KEY UC_age_name (age, name)
+) ENGINE=InnoDB;
+
+create unique index unique_name on person (name);
+
+create table employee (
+  position varchar(50),
+  employee_id INTEGER,
+  CONSTRAINT FK5302D47D93FE702E_diff FOREIGN KEY (employee_id) REFERENCES person (person_id),
+  PRIMARY KEY  (employee_id, position)
+) ENGINE=InnoDB;
+
+create table added (
+  id integer
+);
+
