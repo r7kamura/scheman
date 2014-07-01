1e1f1db264f5e174735a0c02779a098ae49d1e10
Ryo Nakamura <r7kamura@gmail.com>
2014-07-01 23:14:36 +0900

    Add :before_type & :after_type for where the before and after are different type

--- lib/schema_manager/diff.rb
+++ lib/schema_manager/diff.rb
@@ -2,11 +2,16 @@ module SchemaManager
   class Diff
     # @param before [String] The previous schema
     # @param after [String] The next schema
-    # @param type [String] The type of schema syntax (e.g. "mysql")
-    def initialize(before: nil, after: nil, type: nil)
+    # @param type [String] The type of schema syntax, default types of the both schemata (e.g. "mysql")
+    # @param before_type [String] Specify the type of the previous schema if needed
+    # @param after_type [String] Specify the type of the next schema if needed
+    # @raise [SchemaManager::Errors::ParserNotFound]
+    def initialize(before: nil, after: nil, type: nil, before_type: nil, after_type: nil)
       @before = before
       @after = after
       @type = type
+      @before_type = before_type
+      @after_type = after_type
       validate!
     end
 
@@ -19,23 +24,40 @@ module SchemaManager
 
     # @return [SchemaManager::Schema]
     def before_schema
-      @before_schema ||= parser.parse(@before)
+      @before_schema ||= before_parser.parse(@before)
     end
 
     # @return [SchemaManager::Schema]
     def after_schema
-      @after_schema ||= parser.parse(@after)
+      @after_schema ||= after_parser.parse(@after)
     end
 
     # @return [SchemaManager::Parsers::Base]
     # @raise [SchemaManager::Errors::ParserNotFound]
-    def parser
-      @parser ||= ParserBuilder.build(@type)
+    def before_parser
+      @before_parser ||= ParserBuilder.build(before_type)
+    end
+
+    # @return [SchemaManager::Parsers::Base]
+    # @raise [SchemaManager::Errors::ParserNotFound]
+    def after_parser
+      @after_parser ||= ParserBuilder.build(after_type)
     end
 
     # @raise [SchemaManager::Errors::ParserNotFound]
     def validate!
-      parser
+      before_parser
+      after_parser
+    end
+
+    # @return [String]
+    def before_type
+      @before_type || @type
+    end
+
+    # @return [String]
+    def after_type
+      @after_type || @type
     end
   end
 end
