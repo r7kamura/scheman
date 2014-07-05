9891379455a36dfde51645d70118247ea8477706
Ryo Nakamura <r7kamura@gmail.com>
2014-07-06 01:36:01 +0900

    Refactor tests: separates a large example into small pieces

--- spec/schema_manager/parsers/mysql_spec.rb
+++ spec/schema_manager/parsers/mysql_spec.rb
@@ -104,31 +104,119 @@ describe SchemaManager::Parsers::Mysql do
       end
     end
 
-    context "with NOT NULL" do
+    context "with NOT NULL field qualifier" do
       let(:str) do
         "CREATE TABLE `table1` (`column1` INTEGER NOT NULL);"
       end
 
+      it "succeeds in parse field qualifier" do
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :not_null
+      end
+    end
+
+    context "with NULL field qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER NULL);"
+      end
+
       it "succeeds in parse" do
-        should == [
-          {
-            create_table: {
-              name: "table1",
-              fields: [
-                {
-                  name: "column1",
-                  type: "integer",
-                  qualifiers: [
-                    {
-                      type: :not_null,
-                    },
-                  ],
-                },
-              ],
-              constraints: [],
-            },
-          },
-        ]
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :null
+      end
+    end
+
+    context "with AUTO INCREMENT field qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER AUTO INCREMENT);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :auto_increment
+      end
+    end
+
+    context "with PRIMARY KEY field qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER PRIMARY KEY);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :primary_key
+      end
+    end
+
+    context "with UNSIGNED field type qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER UNSIGNED);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers].should be_empty
+      end
+    end
+
+    context "with CHARACTER SET field type qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` VARCHAR(255) CHARACTER SET utf8);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0].should == {
+          type: :character_set,
+          value: "utf8",
+        }
+      end
+    end
+
+    context "with COLLATE field type qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` VARCHAR(255) COLLATE utf8_general_ci);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0].should == {
+          type: :collate,
+          value: "utf8_general_ci",
+        }
+      end
+    end
+
+    context "with UNIQUE KEY field qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER UNIQUE KEY);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :unique_key
+      end
+    end
+
+    context "with UNIQUE INDEX field qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER UNIQUE INDEX);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :unique_key
+      end
+    end
+
+    context "with KEY field qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER KEY);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :key
+      end
+    end
+
+    context "with INDEX field qualifier" do
+      let(:str) do
+        "CREATE TABLE `table1` (`column1` INTEGER INDEX);"
+      end
+
+      it "succeeds in parse" do
+        subject[0][:create_table][:fields][0][:qualifiers][0][:type].should == :key
       end
     end
 
@@ -137,15 +225,7 @@ describe SchemaManager::Parsers::Mysql do
         <<-EOS.strip_heredoc
           CREATE TABLE `table1` (
             `column1` INTEGER NOT NULL AUTO INCREMENT,
-            `column2` VARCHAR(255) NOT NULL,
-            `column3` INTEGER NULL,
-            `column4` INTEGER PRIMARY KEY,
-            `column5` INTEGER UNSIGNED,
-            `column6` VARCHAR(255) CHARACTER SET utf8,
-            `column7` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci,
-            `column8` INTEGER UNIQUE KEY,
-            `column9` INTEGER KEY,
-            PRIMARY KEY (`id`)
+            PRIMARY KEY (`column1`)
           );
         EOS
       end
@@ -168,84 +248,10 @@ describe SchemaManager::Parsers::Mysql do
                     },
                   ],
                 },
-                {
-                  name: "column2",
-                  type: "varchar",
-                  qualifiers: [
-                    {
-                      type: :not_null,
-                    },
-                  ],
-                },
-                {
-                  name: "column3",
-                  type: "integer",
-                  qualifiers: [
-                    {
-                      type: :null,
-                    },
-                  ],
-                },
-                {
-                  name: "column4",
-                  type: "integer",
-                  qualifiers: [
-                    {
-                      type: :primary_key,
-                    },
-                  ],
-                },
-                {
-                  name: "column5",
-                  type: "integer",
-                  qualifiers: [],
-                },
-                {
-                  name: "column6",
-                  type: "varchar",
-                  qualifiers: [
-                    {
-                      type: :character_set,
-                      value: "utf8",
-                    },
-                  ],
-                },
-                {
-                  name: "column7",
-                  type: "varchar",
-                  qualifiers: [
-                    {
-                      type: :character_set,
-                      value: "utf8",
-                    },
-                    {
-                      type: :collate,
-                      value: "utf8_general_ci",
-                    },
-                  ],
-                },
-                {
-                  name: "column8",
-                  type: "integer",
-                  qualifiers: [
-                    {
-                      type: :unique_key,
-                    },
-                  ],
-                },
-                {
-                  name: "column9",
-                  type: "integer",
-                  qualifiers: [
-                    {
-                      type: :key,
-                    },
-                  ],
-                },
               ],
               constraints: [
                 {
-                  primary_key: "id",
+                  primary_key: "column1",
                 },
               ],
             },
