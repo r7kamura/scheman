170611d62f4c4a6ca2ecd81ab5526a1d295866e7
Ryo Nakamura <r7kamura@gmail.com>
2014-07-06 16:15:41 +0900

    Simplify fixture SQL

--- spec/fixtures/example2.sql
+++ spec/fixtures/example2.sql
@@ -1,25 +1,10 @@
-create table person (
-  person_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  name varchar(20) not null,
-  age integer default '18',
-  weight double(11,2),
-  iq int default '0',
-  is_rock_star tinyint default '1',
-  physical_description text,
-  UNIQUE KEY UC_person_id (person_id),
-  UNIQUE KEY UC_age_name (age, name)
-) ENGINE=InnoDB;
-
-create unique index unique_name on person (name);
-
-create table employee (
-  position varchar(50),
-  employee_id INTEGER,
-  CONSTRAINT FK5302D47D93FE702E_diff FOREIGN KEY (employee_id) REFERENCES person (person_id),
-  PRIMARY KEY  (employee_id, position)
-) ENGINE=InnoDB;
-
-create table added (
-  id integer
+CREATE TABLE `table1` (
+  `column1` INTEGER NOT NULL AUTO INCREMENT,
+  `column2` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`column1`)
 );
 
+CREATE TABLE `table2` (
+  `column1` INTEGER NOT NULL AUTO INCREMENT,
+  PRIMARY KEY (`column1`)
+);
